7dca25b8e62c30a96654de4b8d4a92cdafb9ad8e
phil8192 <phil@parasec.net>
2014-05-28 12:56:20 +0300

    CL-USER> (in-package :tsp-art)
    TSP-ART> (time (test-optimise))
    starting distance = 2842782.6950
    optimised distance = 340852.9544
    Evaluation took:
      8.198 seconds of real time
      8.200000 seconds of total run time (8.200000 user, 0.000000 system)
      [ Run times consist of 0.004 seconds GC time, and 8.196 seconds non-GC
    time. ]
      100.02% CPU
      25,441,412,404 processor cycles
      51,868,832 bytes consed

--- tsp-art.lisp
+++ tsp-art.lisp
@@ -1,7 +1,9 @@
 ;; (ql:quickload "png-read")
 ;; (ql:quickload "zpng")
-;; (proclaim '(optimize (debug 3)))
-(proclaim '(optimize (speed 3) (debug 0)))
+
+(defpackage :tsp-art
+  (:use :cl))
+(in-package :tsp-art)
 
 ;; constants for dithering error diffusion filter
 (defconstant +diffusion-e+  7/16 "east pixel")
@@ -159,35 +161,41 @@ y = (0.2126*r) + (0.7152*g) + (0.0722*b)"
   
 ;;________________ tsp.___________________________________________
 
-(defclass point ()
-  ((x
-    :initarg :x
-    :initform (error "need x.")
-    :reader x)
-   (y
-    :initarg :y
-    :initform (error "need y.")
-    :reader y)))
+(deftype vec3 () '(simple-array double-float (3)))
 
-(defclass city(point)
-  ((active
-    :initarg :active
-    :initform t
-    :accessor active-p
-    :documentation "dont-look-bit: if set, the city's adjacent edges
-before and after, will not be considered when searching for a 2-opt move.")))
+(defun make-point (x y active)
+  (let ((vec3 (make-array 3 :element-type 'double-float :initial-element 0.0d0)))
+    (setf (aref vec3 0) x
+          (aref vec3 1) y
+          (aref vec3 2) active)
+    vec3))
 
+(defmacro point-x (point)
+  `(aref ,point 0))
+
+(defmacro point-y (point)
+  `(aref ,point 1))
+
+(defmacro point-active (point)
+  `(aref ,point 2))
+
+(declaim (inline distance-squared))
 (defun distance-squared (p1 p2)
   "for comparing 2 edges."
-  (let ((dx (- (x p1) (x p2)))
-	(dy (- (y p1) (y p2))))
-    (+ (* dx dx) (* dy dy)))) 
+  (declare (optimize (speed 3) (debug 0)))
+  (declare (type vec3 p1 p2))
+  (let ((dx (- (point-x p1) (point-x p2)))
+	(dy (- (point-y p1) (point-y p2))))
+    (+ (* dx dx) (* dy dy))))
 
 (defun distance (p1 p2)
   "euclidean distance."
+  (declare (type vec3 p1 p2))
+  (declare (optimize (speed 3) (debug 0)))
   (sqrt (distance-squared p1 p2)))
 
 (defun tour-distance (points)
+  (declare (optimize (speed 3) (debug 0)))
   (let ((tour-length (length points)))
     (do ((i 1 (1+ i))
 	 (d 0 (+ d (distance (aref points (1- i)) (aref points i)))))
@@ -202,9 +210,9 @@ before and after, will not be considered when searching for a 2-opt move.")))
 	      :for num := (read s nil nil)
 	      :while num
 	      :collect num))))
-    (make-instance 'city
-		   :x (first coordinates)
-		   :y (second coordinates))))
+    (make-point (first coordinates)
+		(second coordinates)
+                1.0d0)))
 
 (defun load-points (file-location)
   "load points from a file into a vector. this vector, when
@@ -217,8 +225,7 @@ iterated in order, represents a tour."
 	 (push (line-to-point line) result))
     (close in)
     (make-array (length result)
-		:initial-contents (reverse result)
-		:element-type 'city)))
+		:initial-contents (reverse result))))
 
 (defun reverse-subseq (array from to)
   (do ((i from (1+ i))
@@ -228,22 +235,32 @@ iterated in order, represents a tour."
       (setf (aref array i) (aref array j))
       (setf (aref array j) tmp))))
 
+(declaim (inline wrap))
 (defun wrap (i max)
+  (declare (optimize (speed 3) (debug 0)))
+  (declare (type fixnum i max))
   (mod (+ i max) max))
 
+(declaim (inline move-cost))
 (defun move-cost (a b c d)
+  (declare (type vec3 a b c d))
+  (declare (optimize (speed 3) (debug 0)))
   (let ((ab (distance-squared a b)) (cd (distance-squared c d))
 	(ac (distance-squared a c)) (bd (distance-squared b d)))
+    (declare (type double-float ab cd ac bd))
     (if (and (< ab ac) (< cd bd))
 	1
 	(- (+ (sqrt ac) (sqrt bd))
-	   (+ (sqrt ab) (sqrt cd))))))
+           (+ (sqrt ab) (sqrt cd))))))
 
 (defun activate (a b c d)
-  (setf (active-p a) t) (setf (active-p b) t)
-  (setf (active-p c) t) (setf (active-p d) t))
+  (declare (type vec3 a b c d))
+  (setf (point-active a) 1.0d0) (setf (point-active b) 1.0d0)
+  (setf (point-active c) 1.0d0) (setf (point-active d) 1.0d0))
 
+(declaim (inline try-move))
 (defun try-move (points from to a b c d)
+  (declare (type vec3 a b c d))                 
   (let ((delta (move-cost a b c d)))
     (if (< delta 0)
 	(progn 
@@ -271,9 +288,6 @@ iterated in order, represents a tour."
 
 ;; nasty.	
 (defun optimise (points)
-
-  (declare (optimize (speed 3) (debug 0) (safety 0)))
-
   (let ((best (tour-distance points))
 	(num-cities (length points))
 	(visited 0)
@@ -281,7 +295,7 @@ iterated in order, represents a tour."
     (do ()
 	((= visited num-cities) best)
       (let ((current-point (aref points current)))
-	(if (active-p current-point)
+	(if (= (point-active current-point) 1.0d0)
 	    (let ((modified (find-move current current-point points num-cities)))
 	      (if (< modified 0)
 		  (progn
@@ -289,7 +303,7 @@ iterated in order, represents a tour."
 		    (setf visited 0)
 		    (incf best modified))
 		  (progn
-		    (setf (active-p current-point) nil)
+		    (setf (point-active current-point) 0.0d0)
 		    (setf current (wrap (1+ current) num-cities))
 		    (incf visited))))
 	    (progn
@@ -297,6 +311,7 @@ iterated in order, represents a tour."
 	      (incf visited)))))))
 
 (defun test-optimise ()
-  (let ((tour-array (load-points "~/greece.points")))
+  (let ((tour-array (load-points "greece.points")))
     (format t "starting distance = ~4$~%" (tour-distance tour-array))
     (format t "optimised distance = ~4$~%" (optimise tour-array))))
+
