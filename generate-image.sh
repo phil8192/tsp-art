0874be0d6eb6188dcc18e455467da9ffe3a29123
phil8192 <phil@parasec.net>
2014-06-02 16:11:48 +0300

    swap width,height

--- generate-image.sh
+++ generate-image.sh
@@ -5,7 +5,7 @@
 # ./generate-image.sh lenna-med-negate.points lenna-med-negate-tsp.png \
 #     1024 1024 1 "#000000" "#ff0000"
 gnuplot -e "unset border; unset xtics; unset ytics;
-            set term pngcairo size $3,$4 enhanced; set output '$2'; 
+            set term pngcairo size $4,$3 enhanced; set output '$2'; 
             set label ''; set key off; 
             set obj 1 rectangle behind from screen 0,0 to screen 1,1; 
             set obj 1 fillstyle solid 1 fillcolor rgbcolor '$6'; 
